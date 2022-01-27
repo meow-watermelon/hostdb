1a6952c4726943bb7b72a6caf7a90a16d70301a7
Hui Li <herdingcat@yahoo.com>
2022-01-27 11:13:02 -0800

    [WIP] - rewrite in class; added Chassis + Product classes
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- hostdb-client.py
+++ hostdb-client.py
@@ -4,41 +4,35 @@ import os
 import platform
 import socket
 
+def read_sysfs_value(sysfs_filename):
+    sysfs_value = None
+
+    try:
+        with open(sysfs_filename, 'rt') as f:
+            sysfs_value = f.readlines()[0].strip()
+    except:
+        pass
+
+    return sysfs_value
+
 class BIOS:
     def __init__(self):
-        self.bios_date = None
-        self.bios_release = None
-        self.bios_vendor = None
-        self.bios_version = None
+        self.bios_date = read_sysfs_value('/sys/devices/virtual/dmi/id/bios_date')
+        self.bios_release = read_sysfs_value('/sys/devices/virtual/dmi/id/bios_release')
+        self.bios_vendor = read_sysfs_value('/sys/devices/virtual/dmi/id/bios_vendor')
+        self.bios_version = read_sysfs_value('/sys/devices/virtual/dmi/id/bios_version')
 
-    def get_bios_info(self):
-        try:
-            with open('/sys/devices/virtual/dmi/id/bios_date', 'rt') as f:
-                data = f.readlines()
-                self.bios_date = data[0].strip()
-        except:
-            pass
+class Chassis:
+    def __init__(self):
+        self.chassis_serial = read_sysfs_value('/sys/devices/virtual/dmi/id/chassis_serial')
+        self.chassis_vendor = read_sysfs_value('/sys/devices/virtual/dmi/id/chassis_vendor')
 
-        try:
-            with open('/sys/devices/virtual/dmi/id/bios_release', 'rt') as f:
-                data = f.readlines()
-                self.bios_release = data[0].strip()
-        except:
-            pass
-
-        try:
-            with open('/sys/devices/virtual/dmi/id/bios_vendor', 'rt') as f:
-                data = f.readlines()
-                self.bios_vendor = data[0].strip()
-        except:
-            pass
-
-        try:
-            with open('/sys/devices/virtual/dmi/id/bios_version', 'rt') as f:
-                data = f.readlines()
-                self.bios_version = data[0].strip()
-        except:
-            pass
+class Product:
+    def __init__(self):
+        self.product_family = read_sysfs_value('/sys/devices/virtual/dmi/id/product_family')
+        self.product_name = read_sysfs_value('/sys/devices/virtual/dmi/id/product_name')
+        self.product_serial = read_sysfs_value('/sys/devices/virtual/dmi/id/product_serial')
+        self.product_sku = read_sysfs_value('/sys/devices/virtual/dmi/id/product_sku')
 
 class Platform:
     def __init__(self):
